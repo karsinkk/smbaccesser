9bcd82fc15526275bb57691f80484f6592392631
rjhua00@sina.com <rjhua00@sina.com@45a1f035-2e49-60a8-c2bf-c0a238443676>
2013-10-14 07:47:20 +0000

    update icon

--- trunk/src/com/eb/client/Constants.java
+++ trunk/src/com/eb/client/Constants.java
@@ -10,4 +10,58 @@ public class Constants {
     public final static String ACTION_START = "com.eb.logcat.start";
     public final static String ACTION_STOP = "com.eb.logcat.stop";
     public final static String LOGCATPATH = "/mnt/sdcard/SmbAccesser/Logcat";
+    public static final String[] VIDEO = new String[] {
+        /* video */
+        ".MP4",
+        ".mp4",
+        ".tts",
+        ".flv",
+        ".avi",
+        ".AVI",
+        ".wmv",
+        ".m4v",
+        ".mpeg",
+        ".mpg",
+        ".MPG",
+        ".ts",
+        ".3gp",
+        ".mkv",
+        ".MKV",
+        ".divx",
+        ".vob",
+        ".VOB",
+        ".mov",
+        ".MOV",
+        ".qt",
+        ".QT",
+        ".dat",
+        ".DAT",
+        ".rm",
+        ".RM",
+        ".rmvb",
+        ".RMVB",
+
+
+    };
+
+    public static final String[] AUDIO = new String[] {
+        /* audio */
+        ".mp3",
+        ".MP3",
+        ".wma",
+        ".wav",
+        ".ogg",
+        ".m4a",
+        ".aac",
+        ".ac3",
+    };
+
+    public static final String[] IMAGE = new String[] {
+        /* picture */
+        ".jpg",
+        ".jpeg",
+        ".png",
+        ".bmp",
+        ".gif",
+    };
 }
