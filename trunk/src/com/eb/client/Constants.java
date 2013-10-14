f4435616ba8b3f3c58e36214a16cf20bb441bf99
rjhua00@sina.com <rjhua00@sina.com@45a1f035-2e49-60a8-c2bf-c0a238443676>
2013-10-14 08:10:30 +0000

    use android API to define path

--- trunk/src/com/eb/client/Constants.java
+++ trunk/src/com/eb/client/Constants.java
@@ -1,15 +1,18 @@
 package com.eb.client;
 
+import android.os.Environment;
+
 public class Constants {
 
     public final static String INFO ="file info";
     public final static String ACTION ="action";
-    public final static String CACHEPATH="/mnt/sdcard/SmbAccesser/Cache/";
-    public final static String COPYPATH="/mnt/sdcard/SmbAccesser/Download/";
+    public final static String ROOT = Environment.getExternalStorageDirectory().getPath() +"/SmbAccesser/";
+    public final static String CACHEPATH=ROOT+"Cache/";
+    public final static String COPYPATH=ROOT+"Download/";
     public final static String ACTION_CMD="com.eb.cmd";
     public final static String ACTION_START = "com.eb.logcat.start";
     public final static String ACTION_STOP = "com.eb.logcat.stop";
-    public final static String LOGCATPATH = "/mnt/sdcard/SmbAccesser/Logcat";
+    public final static String LOGCATPATH = ROOT+"Logcat/";
     public static final String[] VIDEO = new String[] {
         /* video */
         ".MP4",
